881d4fae948d7d608f8e209388ef2c055e7777ad
Backend_Wizard_Wanyonyi <ianwanjala4@gmail.com>
2023-04-24 17:05:20 +0300

    production ready

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,41 @@
 CREATE TABLE IF NOT EXISTS Content(
-    id INTEGER AUTO_INCREMENT,
+    id SERIAL,
     title VARCHAR(150) NOT NULL unique,
     description TEXT,
     status VARCHAR(20) NOT NULL,
     content_type VARCHAR(50) NOT NULL,
     date_created TIMESTAMP NOT NULL,
     date_updated TIMESTAMP,
-    url VARCHAR(255) unique ,
+    url VARCHAR(255) unique,
     primary key(id)
 );
 
-INSERT INTO Content (title, description, status, content_type,
-                     date_created) VALUES ('THE UNRELENTING',
+INSERT INTO content (title, description, status, content_type, date_created) VALUES ('THE UNRELENTING',
                                                               'the story is about a cat that never gave up the chase'
-                                                              , 'IDEA', 'ARTICLE', CURRENT_TIMESTAMP())
+                                                              , 'IDEA', 'ARTICLE', LOCALTIMESTAMP(1));
+INSERT INTO content (title, description, status, content_type, date_created) VALUES ('CHAMPS',
+                                                                                     'the story is about a cat that never gave up the chase'
+                                                                                        , 'IDEA', 'ARTICLE', LOCALTIMESTAMP(1));
+INSERT INTO content (title, description, status, content_type, date_created) VALUES ('VICTORS',
+                                                                                     'the story is about a cat that never gave up the chase'
+                                                                                        , 'IDEA', 'ARTICLE', LOCALTIMESTAMP(1));
+INSERT INTO content (title, description, status, content_type, date_created) VALUES ('BATTLE',
+                                                                                     'the story is about a cat that never gave up the chase'
+                                                                                        , 'IDEA', 'ARTICLE', LOCALTIMESTAMP(1));
+INSERT INTO content (title, description, status, content_type, date_created) VALUES ('DIE HARD',
+                                                                                     'the story is about a cat that never gave up the chase'
+                                                                                        , 'IDEA', 'ARTICLE', LOCALTIMESTAMP(1));
+INSERT INTO content (title, description, status, content_type, date_created) VALUES ('HARD KNOCKS',
+                                                                                     'the story is about a cat that never gave up the chase'
+                                                                                        , 'IDEA', 'ARTICLE', LOCALTIMESTAMP(1));
+INSERT INTO content (title, description, status, content_type, date_created) VALUES ('SHARPENING',
+                                                                                     'the story is about a cat that never gave up the chase'
+                                                                                        , 'IDEA', 'ARTICLE', LOCALTIMESTAMP(1));
+INSERT INTO content (title, description, status, content_type, date_created) VALUES ('AXE',
+                                                                                     'the story is about a cat that never gave up the chase'
+                                                                                        , 'IDEA', 'ARTICLE', LOCALTIMESTAMP(1));
+INSERT INTO content (title, description, status, content_type, date_created) VALUES ('CUT DEEP',
+                                                                                     'the story is about a cat that never gave up the chase'
+                                                                                        , 'IDEA', 'ARTICLE', LOCALTIMESTAMP(1));
+
+
